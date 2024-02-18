eda57e653e6723d55a4c59e238ac22666dd58713
jhongo <jhon_gom22@outlook.com>
2024-02-17 21:17:58 -0500

    🏷️ [ADD] Model Property
    [ADD] Model property for create items

---
+++ lib/infrastructure/models/property.dart
@@ -0,0 +1,30 @@
+
+class Property {
+
+  final String name;
+  final String mainImage;
+  final String address;
+  final double price;
+  final int area;
+  final int bedrooms;
+  final int bathrooms;
+  final int livingrooms;
+  final int buildyears;
+  final int qualitify;
+  final List<String> photos;
+
+  const Property({
+    required this.mainImage, 
+    required this.area, 
+    required this.livingrooms, 
+    required this.buildyears,
+    required this.qualitify,
+    required this.name,
+    required this.address,
+    required this.price,
+    required this.bedrooms,
+    required this.bathrooms,
+    required this.photos
+  });
+  
+}
