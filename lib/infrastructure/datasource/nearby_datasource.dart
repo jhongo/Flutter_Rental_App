f7f4a7ca089e5dab9f41dba134e935bfc96ddebf
jhongo <jhon_gom22@outlook.com>
2024-02-26 23:53:14 -0500

    👔 [ADD]:Logic data
    [ADD]:Logic data

--- lib/infrastructure/datasource/nearby_datasource.dart
+++ lib/infrastructure/datasource/nearby_datasource.dart
@@ -2,9 +2,9 @@ import 'package:flutter_property_rental_ui/infrastructure/models/nearby.dart';
 
 final nearby = <Nearby>[
 
-  Nearby(name: 'Max Apart', image: '', price: '12.000'),
-  Nearby(name: 'Nearby Enclore', image: 'image', price: '14.000'),
-  Nearby(name: 'House Family', image: 'image', price: '15.500'),
-  Nearby(name: 'Modern Apart', image: 'image', price: '20.000'),
+  Nearby(name: 'Max Apart', image: 'assets/imgs/nearby/house_family.jpg', price: '12.000'),
+  Nearby(name: 'Nearby Enclore', image: 'assets/imgs/nearby/max_apart.jpg', price: '14.000'),
+  Nearby(name: 'House Family', image: 'assets/imgs/nearby/modern_apart.jpg', price: '15.500'),
+  Nearby(name: 'Modern Apart', image: 'assets/imgs/nearby/nearby_enclore.jpg', price: '20.000'),
 
 ];
