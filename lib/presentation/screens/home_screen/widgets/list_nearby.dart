83abb7876baed49085ec44e18e1e406c7ef2cd28
jhongo <jhon_gom22@outlook.com>
2024-02-28 23:05:01 -0500

    📱 [ADD] : Desing card vertical.
    [ADD] : Desing card vertical.

--- lib/presentation/screens/home_screen/widgets/list_nearby.dart
+++ lib/presentation/screens/home_screen/widgets/list_nearby.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_property_rental_ui/infrastructure/datasource/nearby_datasource.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ListNearby extends StatelessWidget {
   const ListNearby({super.key});
@@ -9,34 +10,64 @@ class ListNearby extends StatelessWidget {
     return Expanded(
       child: SizedBox(
         width: MediaQuery.of(context).size.width,
-        child: DecoratedBox(
-          decoration: const BoxDecoration(
-            color: Colors.indigo, 
-          ),
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: nearby.length,
-            itemBuilder:(context, index) {
-              return  Padding(
-                padding:const EdgeInsets.fromLTRB(20, 0, 20, 25),
-                child: SizedBox(
-                  width: 270,
-                  // height: 50,
-                  child: DecoratedBox(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.black,
-                    ),
-                    child: Row(
-                      children: [
-                        
-                      ],
-                    ),
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: nearby.length,
+          itemBuilder:(context, index) {
+            final nearbyItem = nearby[index];
+            return  Padding(
+              padding:const EdgeInsets.fromLTRB(20, 0, 20, 25),
+              child: SizedBox(
+                width: 270,
+                // height: 50,
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(18),
+                    color: Colors.white,
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.black12,
+                        blurRadius: 2,
+                        spreadRadius: 2,
+                        offset: Offset(0, 2)
+                      )
+                    ]
+                  ),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: SizedBox(
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(18),
+                            child: DecoratedBox(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(18),
+                              ),
+                              child: Image.asset(nearbyItem.image),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(nearbyItem.name, style: const TextStyle(color: Colors.black, fontSize: 17, fontWeight: FontWeight.normal),),
+                          Text('\$ ${nearbyItem.price}', style: const TextStyle(color: Colors.black, fontSize: 15, fontWeight: FontWeight.bold),),
+                        ],
+                      ),
+                      const Spacer(),
+                      IconButton(
+                        onPressed: (){}, 
+                        icon: const FaIcon(FontAwesomeIcons.heart, color: Colors.black54, size: 20)
+                        )
+                    ],
                   ),
                 ),
-              );
-            }),
-          )
+              ),
+            );
+          })
       ),
     );
   }
